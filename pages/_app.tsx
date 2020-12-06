68a7d5393e14503186f3880bfed7e365477f5e6f
Heavenstone <malolegendrelemaire@gmail.com>
2020-12-06 12:53:16 +0100

    removed default files

---
+++ pages/_app.tsx
@@ -0,0 +1,7 @@
+import "tailwindcss/tailwind.css";
+
+function Wishlist({ Component, pageProps }: any) {
+  return <Component {...pageProps} />;
+}
+
+export default Wishlist;
---
+++ pages/index.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className="container pt-4">
+      <h1 className="text-4xl font-bold text-center">WISHLIST</h1>
+    </div>
+  );
+}
